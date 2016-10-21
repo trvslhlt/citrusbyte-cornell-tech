786e4a8a185ee0eac671cb9b1b0d362ba3381557
trvslhlt <trvslhlt@gmail.com>
2016-10-21 12:36:13 -0400

    show progress bar in notes

--- Scribe/Scribe/ViewControllers/NotesViewController.swift
+++ Scribe/Scribe/ViewControllers/NotesViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class NotesViewController: UIViewController {
 
-    @IBOutlet weak var recordControl: UIButton!
     @IBOutlet weak var notesTableView: UITableView!
     @IBOutlet weak var progressContainer: UIView!
+    var progressViews = [UIView]()
     
     var patient: String!
     var notes = [Note]()
@@ -51,7 +51,50 @@ class NotesViewController: UIViewController {
     }
     
     func setupProgressViews() {
-        print("not done!")
+        let numberOfViews = 7
+        let incrementWidth = UIScreen.main.bounds.width / CGFloat(numberOfViews)
+        let viewHeight: CGFloat = 50
+        let viewSize = CGSize(width: incrementWidth, height: viewHeight)
+        for i in 0..<numberOfViews {
+            let origin = CGPoint(x: CGFloat(i) * incrementWidth, y: 0)
+            let view = UIView(frame: CGRect(origin: origin, size: viewSize))
+            view.tag = i
+            progressContainer.addSubview(view)
+            view.backgroundColor = UIColor.green
+            progressViews.append(view)
+        }
+    }
+    
+    func updateProgressbar() {
+        let portion = CGFloat(notes.count) / 7
+        let mixedColor = blendColor(color1: UIColor.green, withColor: UIColor.red, portion: portion)
+        for view in progressViews {
+            view.backgroundColor = mixedColor
+            if view.tag < notes.count {
+                view.isHidden = false
+            } else {
+                view.isHidden = true
+            }
+        }
+    }
+    
+    func blendColor(color1: UIColor, withColor color2: UIColor, portion: CGFloat) -> UIColor {
+        var r1: CGFloat = 0
+        var g1: CGFloat = 0
+        var b1: CGFloat = 0
+        var a1: CGFloat = 0
+        var r2:CGFloat = 0
+        var g2:CGFloat = 0
+        var b2:CGFloat = 0
+        var a2:CGFloat = 0
+        
+        color1.getRed(&r1, green: &g1, blue: &b1, alpha: &a1)
+        color2.getRed(&r2, green: &g2, blue: &b2, alpha: &a2)
+        
+        return UIColor(red: r1 * portion +  r2 * (1 - portion),
+                       green: g1 * portion + g2 * (1 - portion),
+                       blue: b1 * portion + b2 * (1 - portion),
+                       alpha: max(a1, a2))
     }
 
 }
@@ -69,6 +112,7 @@ extension NotesViewController: NoteComposerDelegate {
 extension NotesViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        updateProgressbar()
         return notes.count
     }
     
